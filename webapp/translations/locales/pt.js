bacac30bf6a0f53962766f910352d64a73f48f96
MicroHuchaSolidaria <info@microhuchasolidaria.org>
2017-05-31 16:19:11 +0200

    Update pt.js

--- webapp/translations/locales/pt.js
+++ webapp/translations/locales/pt.js
@@ -1,5 +1,5 @@
 const localizations = {
-  'hi': '',
+  'hi': 'Olá',
   'Thanks for your generous heart. You are changing the world for a lot of people who lost hope': '',
   'Login successfully!': '',
   'Loading': '',
