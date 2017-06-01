367a11b59e3dc4b1b46c48ef7721ae57e85e9a49
Mahdi Bolow <boolow5@gmail.com>
2017-06-01 09:25:50 +0300

    added some data variables and calculate method

--- webapp/src/components/Subscription.vue
+++ webapp/src/components/Subscription.vue
@@ -106,7 +106,10 @@ export default {
       cardNo: '',
       CVV: '',
       expirationDate: { month: thisMonth, year: thisYear },
-      registerCardResponse: {}
+      registerCardResponse: {},
+      fees: 0,
+      tax: 0,
+      months: 1
     }
   },
   computed: {
@@ -115,174 +118,8 @@ export default {
     }
   },
   methods: {
-    registerCard () {
-      if (!this.rechargeFormIsValid()) {
-        return
-      }
-      var jwtToken = localStorage.getItem('user_token')
-      var vm = this
-
-      // this.$http.headers.common['Authorization'] = 'Bearer ' + jwtToken
-      var authorizationHeader = 'Bearer ' + jwtToken
-      var options = {
-        url: urls.API_URL.CurrentUrl + urls.REGISTER_CARD_URL,
-        method: 'POST',
-        headers: { 'Authorization': authorizationHeader }
-      }
-      this.$http(options).then(resp => {
-        if (resp.data) {
-          vm.$store.commit('setRegCardResponse', resp.data)
-          // vm.$store.commit('setSuccess', 'Register card successful')
-          // make the registerCard call with the response data
-          vm.callToMangoPayToGetToken(resp.data)
-        } else {
-          if (resp.error) {
-            vm.$store.commit('setError', {error: resp.error})
-          } else {
-            vm.$store.commit('setError', {error: 'Not data in resp'})
-          }
-        }
-      }, err => {
-        if (!err.data) {
-          vm.$store.commit('setError', {error: 'Card registeration error'})
-          return
-        }
-        if (err.data.errors) {
-          vm.$store.commit('setErrors', err.data.errors)
-        }
-        vm.$store.commit('setLoading', false)
-      })
-    },
-    callToMangoPayToGetToken (data) {
-      // resp.data.accessKeyRef, resp.data.cardRegistrationURL, resp.data.data
-      this.sendToMangopay(data.accessKeyRef, data.cardRegistrationURL, data.data, this.cardNo, this.expirationDate, this.CVV, this.rechargeAccount)
-    },
-    sendToMangopay (accessKeyRef, cardRegistrationURL, data, cardNo, expirationDate, CVV, rechargeAccountCallBack) {
-      var mangopayData = {
-        data: data,
-        accessKeyRef: accessKeyRef,
-        cardNumber: cardNo,
-        cardExpirationDate: expirationDate.month + expirationDate.year, // MMYY
-        cardCvx: CVV
-      }
-      var mangoParameters = ''
-      for (var key in mangopayData) {
-        mangoParameters += (mangoParameters.length > 0 ? '&' : '') + key + '=' + encodeURIComponent(mangopayData[key])
-      }
-
-      // var jwtToken = localStorage.getItem('user_token')
-      var vm = this
-
-      // this.$http.headers.common['Authorization'] = 'Bearer ' + jwtToken
-      // delete this.$http.headers.common['Authorization']
-      // console.log('headers:')
-      // console.log(this.$http.headers)
-      // 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
-      // var options = {
-      //     data:         mangopayData,
-      //     emulateJSON:  mangoParameters,
-      //     headers:      { 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8' }
-      // }
-      // this.$http.post(cardRegistrationURL, mangopayData, options).then(resp => {
-      //     rechargeAccountCallBack(resp.data)
-      //   }, err => {
-      //     vm.$store.commit('setLoading', false)
-      //   })
-      var instance = axios.create({
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
-      })
-      instance.post(cardRegistrationURL, mangopayData).then(resp => {
-        rechargeAccountCallBack(resp.data)
-      }).catch(err => {
-        vm.$store.commit('setLoading', false)
-        vm.$store.commit('setError', 'Error occured while waiting for the payments service')
-        console.log(err)
-      })
-    },
-    rechargeAccount (token) {
-      token = 'data=' + token
-      // var jwtToken = localStorage.getItem('user_token')
-      var vm = this
-      var rechargeData = { amount: parseFloat(this.amount) * 100, token: token }
-
-      let url = urls.API_URL.CurrentUrl + urls.RECHARGE_ACCOUNT_URL
-      axios({
-        method: 'post',
-        url: url,
-        data: rechargeData,
-        headers: { 'Authorization': 'Bearer ' + jwtToken }
-      }).then(resp => {
-        vm.$store.commit('setLoading', false)
-        vm.$events.$emit('acountUpdate', {})
-        vm.$store.commit('setSuccess', 'Recharge successful')
-      }).catch(err => {
-        console.log('Recharge request error')
-        console.log(err)
-        if (err.error) {
-          vm.$store.commit('setError', err.error)
-        } else if (err.errors) {
-          vm.$store.commit('setErrors', err.errors)
-        } else if (err.data.errors) {
-          vm.$store.commit('setErrors', err.data.errors)
-        } else if (err.data.error) {
-          vm.$store.commit('setError', err.data.error)
-        } else {
-          vm.$store.commit('setError', err)
-        }
-
-        vm.$store.commit('setLoading', false)
-      })
-
-      // this.$http.headers.common['Authorization'] = 'Bearer ' + jwtToken
-      //
-      // this.$http.post(url, rechargeData)
-      //   .then(resp => {
-      //     vm.$store.commit('setLoading', false)
-      //     vm.$events.$emit('acountUpdate', {})
-      //     vm.$store.commit('setSuccess', 'Recharge successful')
-      //   }, err => {
-      //     if (err.error) {
-      //       vm.$store.commit('setError', err.error)
-      //     } else if (err.errors) {
-      //       vm.$store.commit('setErrors', err.errors)
-      //     } else if (err.data.errors) {
-      //       vm.$store.commit('setErrors', err.data.errors)
-      //     } else if (err.data.error) {
-      //       vm.$store.commit('setError', err.data.error)
-      //     } else {
-      //       vm.$store.commit('setError', 'Error recharging account')
-      //     }
-      //
-      //     vm.$store.commit('setLoading', false)
-      //   })
-    },
-
-    rechargeFormIsValid () {
-      var isValid = true
-      // if (this.amount > parseFloat(this.$store.getters.getBalance)) {
-      //   this.$store.commit('setError', 'Amount you want to recharge is greater than your balance')
-      // }
-      if (this.cardNo.length < 16) {
-        this.$store.commit('setError', 'The card number is too short')
-        isValid = false
-      }
-      var thisYear = new Date().getFullYear()
-      var y = thisYear - 2000
-      if (this.expirationDate.month < 1 && this.expirationDate.month > 12 || this.expirationDate.year < y) {
-        this.$store.commit('setError', 'The expiration date is either invalid or already expired')
-        isValid = false
-      }
-
-      return isValid
-    },
-
-    registerCardAndRecharge () {
-      if (!this.rechargeFormIsValid()) {
-        return
-      }
-      this.$store.commit('setLoading', true)
-      // start the process that calls other calls until the recharge is complete
-      this.registerCard()
+    calculateFees () {
+      var amount = 10 * this.months * 100
     }
   }
 }
