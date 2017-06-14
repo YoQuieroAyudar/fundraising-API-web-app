ba6f8624553d87238d0c9b2b18cf5fd5f5430981
Mahdi Bolow <boolow5@gmail.com>
2017-06-14 12:18:16 +0300

    restyled the share buttons into tiles (mosaic)

--- webapp/src/components/Share.vue
+++ webapp/src/components/Share.vue
@@ -7,65 +7,89 @@
                       :hashtags="$t(hashtags)"
                       :twitter-user="$t(outTwitterUser)"
                       v-cloak inline-template>
+
                     <div>
-                      <ul class="list-unstyled share-buttons-list" style="font-size: 1.65em;">
-                        <li class="">
-                          <a href="#">
-                            <network network="facebook">
-                              <i class="fa fa-facebook"></i> Facebook
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="googleplus">
-                              <i class="fa fa-google-plus"></i> Google +
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="linkedin">
-                              <i class="fa fa-linkedin"></i> LinkedIn
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="pinterest">
-                              <i class="fa fa-pinterest"></i> Pinterest
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="reddit">
-                              <i class="fa fa-reddit"></i> Reddit
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="twitter">
-                              <i class="fa fa-twitter"></i> Twitter
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="vk">
-                              <i class="fa fa-vk"></i> VKontakte
-                            </network>
-                          </a>
-                        </li>
-                        <li class="">
-                          <a href="#">
-                            <network network="whatsapp">
-                              <i class="fa fa-whatsapp"></i> Whatsapp
-                            </network>
-                          </a>
-                        </li>
-                      </ul>
+
+                      <div class="container" style="margin-top: 2em;">
+                        <div class="row">
+                          <div style="height:4em;border: 1px solid #fff;background-color:#3b5998;" class="col-xs-6">
+                            <a href="#">
+                              <network network="facebook">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-facebook"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                          <div style="height:4em;border: 1px solid white;background-color:#55acee;" class="col-xs-6">
+                            <a href="#">
+                              <network network="twitter">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-twitter"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                        </div>
+
+                        <div class="row">
+                          <div style="height:4em;border: 1px solid white;background-color:#4dc247;" class="col-xs-6">
+                            <a href="#">
+                              <network network="whatsapp">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-whatsapp"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                          <div style="height:4em;border: 1px solid white;background-color:#dd4b39;" class="col-xs-6">
+                            <a href="#">
+                              <network network="googleplus">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-google-plus"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                        </div>
+
+                        <div class="row">
+
+                          <div style="height:4em;border: 1px solid white;background-color:#cb2027;" class="col-xs-6">
+                            <a href="#">
+                              <network network="pinterest">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-pinterest"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                          <div style="height:4em;border: 1px solid white;background-color:#007bb5;" class="col-xs-6">
+                            <a href="#">
+                              <network network="linkedin">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-linkedin"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                        </div>
+
+                        <div class="row">
+
+                          <div style="height:4em;border: 1px solid white;background-color:#ff4500;" class="col-xs-6">
+                            <a href="#">
+                              <network network="reddit">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-reddit"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                          <div style="height:4em;border: 1px solid white;background-color:#45668e;" class="col-xs-6">
+                            <a href="#">
+                              <network network="vk">
+                                <i style="font-size: 2em;margin:0.5em 1em;color:white;" class="fa fa-vk"></i>
+                              </network>
+                            </a>
+                          </div>
+
+                        </div>
+
+                      </div>
 
                   </div>
     </social-sharing>
@@ -93,4 +117,5 @@ export default {
 a.social-link:hover {
   border: 1px solid #000;
 }
+
 </style>
