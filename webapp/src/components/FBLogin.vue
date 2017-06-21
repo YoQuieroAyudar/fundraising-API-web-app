04ab0ff823e80c6f8aa3ac5c8ffcfee00dc3dad7
Mahdi Bolow <boolow5@gmail.com>
2017-06-21 15:31:19 +0300

    added a facebook login

--- webapp/src/components/FBLogin.vue
+++ webapp/src/components/FBLogin.vue
@@ -1,69 +1,130 @@
-<!-- <template>
-  <div v-if="!authenticated">
-    <a href="#" @click='doLogin' class="btn btn-block btn-social btn-facebook btn-flat">
-      <i class="fa fa-facebook"></i> {{ $t('Sign in using Facebook') }}
-    </a>
+<template>
+  <div class="">
+    <div v-if="ready" class="">
+        <button v-if="authorized" @click="logout()" class="btn btn-danger"> <i class="fa fa-facebook fa-fw"></i> {{$t('Logout from facebook')}}</button>
+        <button v-else="authorized" @click="login()" class="btn btn-primary"> <i class="fa fa-facebook fa-fw"></i> {{$t('Login from facebook')}}</button>
+
+        <div v-if="authorized" class="">
+
+        </div>
+      </div>
   </div>
 </template>
 
 <style scoped>
-.fb-login-button {
-  margin-bottom: .3em;
+html, body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #EDF7FF;
+  text-align: center;
+  padding: 30px;
+}
+.box {
+  background-color: #B6E3F6;
+  margin-bottom: 20px;
+  padding: 20px;
+  border-radius: 5px;
 }
 </style>
 
 <script>
-import auth from './../auth'
 export default {
 
   data () {
     return {
-      email: '',
-      password: '',
-      authenticated: false
+      profile: {},
+      ready: false,
+      authorized: false
     }
   },
-  computed: {
-
+  mounted () {
+    console.log('FACEBOOK LOGIN mounted')
+    let vm = this
+    window.fbAsyncInit = () => {
+      console.log('FACEBOOK LOGIN fbAsyncInit')
+      /*eslint-disable */
+      window.FB.init({
+        appId: '219601041795109',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.9'
+      })
+      window.FB.getLoginStatus(function (response) {
+        console.log('FACEBOOK LOGIN getLoginStatus')
+        console.log(response)
+        vm.statusChangeCallback(response)
+      })
+      /*eslint-enable */
+    }
+    setTimeout(function () {
+      window.fbAsyncInit()
+    }, 1000)
   },
   beforeCreate () {
-    this.loadFbLoginApi()
+    console.log('FACEBOOK LOGIN beforeCreate')
   },
   methods: {
-    loadFbLoginApi () {
-      window.fbAsyncInit = function () {
-        FB.init({
-          appId      : FB_APP_ID,
-          cookie     : true,  // enable cookies to allow the server to access
-          // the session
-          xfbml      : true,  // parse social plugins on this page
-          version    : 'v2.5' // use version 2.1
-        })
-      }
-
-      console.log('Loading fb api')
-        // Load the SDK asynchronously
-      (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0]
-          if (d.getElementById(id)) return
-          js = d.createElement(s);  js.id = id
-          js.src = '//connect.facebook.net/en_US/sdk.js'
-          fjs.parentNode.insertBefore(js, fjs)
-      }(document, 'script', 'facebook-jssdk'))
-    },
-    doLogin () {
-      var vm = this
-
-      FB.login(function (response) {
-        if (response.status === 'connected') {
-          console.log('In response')
-          auth.facebookLogin(response.authResponse, function () {
-            vm.$router.push({ name: 'redirect' })
-          })
-        }
+    getProfile () {
+      console.log('FACEBOOK LOGIN getProfile()')
+      /*eslint-disable */
+      let vm = this
+      window.FB.api('/me', function (response) {
+        console.log('getProfile response')
+        console.log(response)
+        // vm.$set(vm, 'profile', response)
+        vm.$store.commit('setFBProfile', response)
       })
+      /*eslint-enable */
+    },
+    login () {
+      console.log('FACEBOOK LOGIN login()')
+      let vm = this
+      /*eslint-disable */
+      FB.login(function (response) {
+        console.log('login response')
+        console.log(response)
+        vm.statusChangeCallback(response)
+      }, {scope: 'publish_actions'})
+      /*eslint-enable */
+    },
+    logout () {
+      console.log('FACEBOOK LOGIN logout()')
+      let vm = this
+      /*eslint-disable */
+      FB.logout(function (response) {
+        console.log('logout response')
+        console.log(response)
+        vm.statusChangeCallback(response)
+      })
+      /*eslint-enable */
+    },
+    statusChangeCallback (response) {
+      console.log('FACEBOOK LOGIN statusChangeCallback')
+      let vm = this
+      vm.ready = true
+      console.log('statusChangeCallback')
+      console.log(response)
+      if (response.status === 'connected') {
+        console.log('authResponse')
+        console.log(response.authResponse)
+        vm.$events.emit('socialLoginEvent', {
+          type: 'facebook',
+          id: response.authResponse.accessToken
+        })
+        vm.$store.commit('setFBAuthorized', true)
+        vm.authorized = true
+        vm.getProfile()
+      } else if (response.status === 'not_authorized') {
+        vm.authorized = false
+        vm.$store.commit('setFBAuthorized', false)
+      } else {
+        vm.authorized = false
+        vm.$store.commit('setFBAuthorized', false)
+      }
     }
   }
 
 }
-</script> -->
+</script>
