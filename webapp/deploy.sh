9f23ab9570562800d703a0e8d9c1e56fbdddf87b
mahdi bolow <boolow5@gmail.com>
2017-07-10 15:50:41 +0300

    moved the sync command on its own script and add > fundraising-webapp@1.0.0 sync /home/mahdi/YoQuieroAyudar/fundraising-API-web-app/webapp > bash sync-all.sh
    run sync command in the server
    syncing JVA in /var/www/web-jva
    Already up-to-date.
    syncing MHS in /var/www/web-mhs
    Already up-to-date.
    syncing IWTH in /var/www/web-iwth
    Already up-to-date.
    done syncing the server web apps

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,11 +1,4 @@
 TARGET_DIRECTORY=../../YoQuieroAyudar.github.io/
-HOST="api.microhuchasolidaria.org"
-USER="deploy"
 echo "Copying files to "$TARGET_DIRECTORY
 cp -r dist/* $TARGET_DIRECTORY
 echo 'Done copying'
-echo 'run sync command in the server'
-ssh $USER@HOST << EOF
-  bash ~/sync-all-web.sh
-EOF
-echo "done syncing the server web apps"
